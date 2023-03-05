8545bd62b7c52d6177950075a84b2cf8e0a15465
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-03-05 00:41:20 -0300

    Correct jsdocs and implement add operation

--- src/crud/firebaseOperations.js
+++ src/crud/firebaseOperations.js
@@ -1,16 +1,20 @@
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs, addDoc } from 'firebase/firestore';
 import { db } from '../config/firebase.js';
 import { errorHandler } from '../utils/firebaseErrorHandler.js';
 
 //TODO Find better location to put these methods.
 
 /** Get all documents from one collection
- * @param {String} collection
+ * @param {String} firebaseCollectionName
  * @returns {object} returns all data from x collection
+ * ```
+ * const addRecommendedTrip = await listAllDocsFromCollection('collectionName');
+ * addRecommendedTrip;
+ * ```
  */
-export const listAllDocsFromCollection = async (fCollection) => {
+export const listAllDocsFromCollection = async (firebaseCollectionName) => {
     try {
-        const getCollection = collection(db, fCollection);
+        const getCollection = collection(db, firebaseCollectionName);
         const docsFromCollection = await getDocs(getCollection);
         if (docsFromCollection != []) {
             const docsList = docsFromCollection.docs.map((doc) => doc.data());
@@ -27,13 +31,19 @@ export const listAllDocsFromCollection = async (fCollection) => {
 };
 
 /** Get one single documents from one collection that matches with given id
- * @param {String} collection
+ * @param {String} firebaseCollectionName
  * @param {String} documentId
  * @returns {object} returns the matching document data from x collection
+ * ```
+ * const listSingleUserDocument = await listDocFromCollectionWithId('user', userId)
+ * ```
  */
-export const listDocFromCollectionWithId = async (fCollection, docId) => {
+export const listDocFromCollectionWithId = async (
+    firebaseCollectionName,
+    documentId,
+) => {
     try {
-        const getDocument = await doc(db, fCollection, docId);
+        const getDocument = await doc(db, firebaseCollectionName, documentId);
         const document = await getDoc(getDocument);
         if (document.exists()) {
             return document.data();
@@ -43,6 +53,30 @@ export const listDocFromCollectionWithId = async (fCollection, docId) => {
                 'No such document. Check if the collection or document names are inserted correctly.',
             );
         }
+    } catch (error) {
+        errorHandler(error);
+    }
+};
+
+/** Add a document with an auto-generated id to certain collection
+ * @param {String} firebaseCollectionName
+ * @param {object} docData
+ * @returns {object} returns the matching document data from x collection
+ * @example
+ * ```
+ * const addRecommendedTrip = await addDocInCollection('test', {
+ *  documentField01: 'Hello',
+ *  documentField02: 'world'
+ * });
+ * addRecommendedTrip;
+ * ```
+ */
+export const addDocInCollection = async (firebaseCollectionName, docData) => {
+    try {
+        const docRef = await addDoc(collection(db, firebaseCollectionName), {
+            ...docData,
+        });
+        return docRef;
     } catch (error) {
         errorHandler(error);
     }
