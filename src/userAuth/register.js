257d3afb5f968b5a5d019f2ed1d2f30d6546e88a
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-03-05 17:10:51 -0300

    Delete unused function

--- src/userAuth/register.js
+++ src/userAuth/register.js
@@ -6,27 +6,6 @@ import { auth } from '../config/firebase.js';
 import { addDocInCollection } from '../crud/firebaseOperations.js';
 import { errorHandler } from '../utils/firebaseErrorHandler.js';
 
-/**
- *  Checks if user is registered in firebase auth and returns all the user information in an object.
- *  @param {String} email
- *  @param {String} password
- *  @returns {CurrentUserInfo} current user object
- *
- */
-
-export const signIn = async (email, password) => {
-    const response = signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            let user = userCredential.user;
-            console.log(`User logged in! ${email}`);
-            return user;
-        })
-        .catch((error) => {
-            errorHandler(error);
-        });
-    return response;
-};
-
 //TODO Complete research over required user information
 /**
  *  Register user in firebase auth and storage, using its email, password, name, address
