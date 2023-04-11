205dd6f9e50a6fe945551cf0351f2c98b72e87a9
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-04-11 17:07:06 -0300

    Make login return custom token
    It will make possible to the frontend consume the token and persist the user

--- src/models/firebase/userAuth/login.js
+++ src/models/firebase/userAuth/login.js
@@ -1,27 +1,63 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import serviceAccount from "../../../donotcommit/trip-planner-b0cfe-firebase-adminsdk-e4r7b-d2eb2a11d7.json"  assert {type: 'json'};
+import { getAuth, signInWithCustomToken, signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../../config/firebase.js';
 import { logger } from '../../../utils/logger.js';
+import admin from 'firebase-admin'
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 /**
- *  Checks if user is registered in firebase auth and returns all the user information in an object.
+ *  Checks email and password using auth and returns token 
  *  @param {String} email
  *  @param {String} password
  *  @returns {CurrentUserInfo} current user object
  *
  */
 export const signIn = async (userInformation) => {
-    const { email, currentPassword } = userInformation;
-    const response = signInWithEmailAndPassword(auth, email, currentPassword)
+    const { email, password } = userInformation;
+    const authenticateUser = await signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             let user = userCredential.user;
-            logger.info('User logged in! ' + user.email);
-            return { status: 200, message: user };
+            logger.info(`User authenticated: ${user.email}`);
+            return { status: 200, message: user.uid };
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
             logger.error(`Error code ${errorCode}: ${errorMessage}`);
-            throw new Error(error);
+            throw new Error(error)
         });
-    return response;
+
+    const userId = authenticateUser.message;
+    let token = await admin.auth().createCustomToken(userId).then((customToken) => {
+        logger.info('Token generated');
+        return customToken;
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        logger.error(`Error code ${errorCode}: ${errorMessage}`);
+        throw new Error(error)
+    });
+
+    return token;
 };
+
+// const a = await signIn({ email: "lauro@lauro.com", password: "lauro123" })
+
+// const { message } = await a
+
+// const teste = await signInWithCustomToken(getAuth(), await message)
+//     .then((userCredential) => {
+//         // Signed in
+//         var user = userCredential;
+//         console.log("User Credentials: " + JSON.stringify(user));
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         logger.error(`Error code ${errorCode}: ${errorMessage}`);
+//         throw new Error(error)
+//     })
