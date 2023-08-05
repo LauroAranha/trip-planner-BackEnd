92e007d1ae05a01c0e3e6b7f037060cad19f0d90
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-08-05 11:28:40 -0300

    refactor code

--- src/models/firebase/userAuth/register.js
+++ src/models/firebase/userAuth/register.js
@@ -1,44 +1,30 @@
-import {
-    createUserWithEmailAndPassword,
-} from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { getStorage, ref, uploadString } from "firebase/storage";
 import { auth } from '../../../config/firebase.js';
 import admin from '../../../config/admin.js';
 import { addDocInCollection } from '../firebaseOperations.js';
 import { errorHandler } from '../../../utils/firebaseErrorHandler.js';
-
 import { logger } from '../../../utils/logger.js';
 
 export const signUp = async (userInformation) => {
     const { email, password } = userInformation;
+
     try {
-        await createUserWithEmailAndPassword(auth, email, password)
-            .then(async (userCredential) => {
-                // Signed up succesfully
-                const user = userCredential.user;
-                logger.info(`User created on auth succesfully! ${email}`);
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
 
-                await admin
-                    .auth()
-                    .getUserByEmail(email)
-                    .then((userRecord) => {
-                        // See the UserRecord reference doc for the contents of userRecord.
-                        let authUid = userRecord.uid;
-                        console.log(
-                            `Successfully fetched user data: ${userRecord.toJSON()}`,
-                        );
+        logger.info(`User created on auth successfully! ${email}`);
 
-                        userInformation.userId = authUid;
-                        addDocInCollection('users', userInformation);
-                    })
-                    .catch((error) => {
-                        console.log(`Error fetching user data: ${error}`);
-                    });
-                return user;
-            })
-            .catch((error) => {
-                errorHandler(error);
-            });
+        const userRecord = await admin.auth().getUserByEmail(email);
+        let authUid = userRecord.uid;
+        console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+
+        userInformation.userId = authUid;
+        await addDocInCollection('users', userInformation);
+
+        return user;
     } catch (error) {
         errorHandler(error);
     }
 };
+
