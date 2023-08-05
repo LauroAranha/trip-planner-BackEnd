55a78d3fdeaba92e373550563ace5699272417b5
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-08-05 00:16:03 -0300

    remove deprecated jsdoc

--- src/models/firebase/userAuth/register.js
+++ src/models/firebase/userAuth/register.js
@@ -8,24 +8,6 @@ import { errorHandler } from '../../../utils/firebaseErrorHandler.js';
 
 import { logger } from '../../../utils/logger.js';
 
-//TODO Complete research over required user information
-/**
- *  Register user in firebase auth and storage, using its email, password, name, address
- *  @typedef signUp
- *  @param {object} userInformation
- *  @param {String} userInformation.email
- *  @param {String} userInformation.name
- *  @param {String} userInformation.address
- *  @param {String} currentPassword
- *  @param {String} confirmPassword
- *  @returns {CurrentUserInfo} current user object
- *  ```
- *  const signUpUser = await signUp({
- *    email: 'test@test.com', name: 'mockup', address: street'
- *    },'password123');
- *  signUpUser
- *  ```
- */
 export const signUp = async (userInformation) => {
     const { email, currentPassword } = userInformation;
     try {
