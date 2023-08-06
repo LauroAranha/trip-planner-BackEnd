bda11da3a4976a358a80b58cdfd97722caf2fb3b
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-08-06 13:48:02 -0300

    fix request too large bug

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 import userRoutes from './routes/userRoutes.js';
 import roadmapRoutes from './routes/roadmapRoutes.js'
 
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
